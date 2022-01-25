eff9c7a64d9b578457aa5dd40caa0d57befac056
AYAM-BLESS <blessbissong@gmail.com>
2022-01-24 23:37:40 -0800

    code

---
+++ Fahrenheit_to_celcius.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int fahr, celcus;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    while(fahr<=upper){
+        celcus = 5*(fahr-32)/9;
+        printf("%d\t%d\n",fahr, celcus);
+        fahr = fahr + step;
+    }
+}
