3f059755424a8fe790e226e7b2c5ecb24fb304f2
AYAM-BLESS <blessbissong@gmail.com>
2022-02-08 11:29:20 -0800

    gets a number and display the sqare root

---
+++ square_root.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num;
+    printf("Enter a number to get the square root\n",num);
+    scanf("%d", &num);
+    printf("Square root of number is = %f", sqrt(num));
+    return 0;
+}
