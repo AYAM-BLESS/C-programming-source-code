2751daff67b8677ab83d0dc83c9a96ceff59f5e9
AYAM-BLESS <blessbissong@gmail.com>
2022-02-07 19:14:25 -0800

    adding of two static values

---
+++ addition.c
@@ -0,0 +1,14 @@
+// done by ayamba bless on the 07/02/2022
+
+// addition of static values
+
+
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    a = 12;
+    b = 4;
+    c = a + b;
+    printf("c = %d",c);
+    return 0;
+}
