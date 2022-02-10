098cb7e48831c32d0e5b2db67b6926db640791e7
AYAM-BLESS <blessbissong@gmail.com>
2022-02-09 18:19:12 -0800

    printing an integer value entered by the user

---
+++ printing_integer.c
@@ -0,0 +1,10 @@
+//dome by ayamba bless on the 09/02/2022
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter an integer number\n",a);
+    scanf("%d", &a);
+    printf("You have entered the number: %d\n", a);
+    return 0;
+}
