bfc4b74deadfd87210016899f9f78ce3e3cc43a1
AYAM-BLESS <blessbissong@gmail.com>
2022-02-17 22:34:12 -0800

    prime number

---
+++ prime.c
@@ -0,0 +1,16 @@
+//done by ayamba bless
+
+
+#include<stdio.h>
+int main()
+{
+    int num, i, count=0;
+    printf("Enter a number : ", num);
+    scanf("%d", &num);
+
+    if(count==0 && num!=1)
+    printf("%d is a prime number", num);
+    else
+    printf("%d is not a prime number", num);
+    return 0;
+}
