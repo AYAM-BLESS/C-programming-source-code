c3a42d826d3dd8f9705d1ad0fcb442c26c909b64
AYAM-BLESS <blessbissong@gmail.com>
2022-03-09 09:17:31 -0800

    task 2

---
+++ task2.c
@@ -0,0 +1,11 @@
+//done by ayamba bless
+#include<stdio.h>
+int main(){
+    int i = 2, j= 3;
+    {
+        int i =4, j=5;
+        printf("%d%d", i, j);
+    }
+    printf("%d%d",i,j);
+    return 0;
+}
