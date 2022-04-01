a7e7b9c4c87d22b5ee9c254cb6fd2fbe89b5b03e
AYAM-BLESS <blessbissong@gmail.com>
2022-03-31 21:28:28 -0700

    task for the users to determine the output

---
+++ task1.c
@@ -0,0 +1,9 @@
+//done by ayamba bless
+//what is the output of the code below
+// 29 or 10.25 or 10 or compiler error
+#include<stdio.h>
+int main (){
+    int x = 5 * 2 / 8 + 9 ;
+    printf("%d",x);
+    return 0;
+}
