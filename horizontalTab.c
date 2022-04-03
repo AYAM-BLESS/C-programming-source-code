68d722da7294e7e0e9cf6abb1670fe9607ed962b
AYAM-BLESS <blessbissong@gmail.com>
2022-04-02 22:49:36 -0700

    Horizontal Tab

---
+++ horizontalTab.c
@@ -0,0 +1,9 @@
+//done by Ayamba Bless
+// Horizontal Tab
+#include<stdio.h>
+int main(){
+    printf("\t horizontal tab escape sequence");
+    printf("\n 34567 \t 34567");
+    printf("\n 123   \t 678");
+    return 0;
+}
