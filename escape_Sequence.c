197fec31a933cb12ba7866344949eff7c9dba916
AYAM-BLESS <blessbissong@gmail.com>
2022-04-02 22:42:58 -0700

    the new line character

---
+++ escape_Sequence.c
@@ -0,0 +1,9 @@
+//done by Ayamba Bless
+//New Line
+#include<stdio.h>
+int main(){
+    printf("\n new line escape sequence");
+    printf("\n first line");
+    printf("\n second line");
+    return 0;
+}
