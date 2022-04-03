2aa6fec4e753398b4ff5c142261e64508844fca1
AYAM-BLESS <84291403+AYAM-BLESS@users.noreply.github.com>
2022-04-03 21:21:39 +0100

    Add files via upload
    Question mark sequence

---
+++ question mark.c
@@ -0,0 +1,11 @@
+// done by Ayamba Bless
+// question mark sequence
+
+#include <stdio.h>
+int main ()
+{
+printf("\n below is the demo");
+printf(" \n what’s the price of one kg of tomatoes \? ");
+printf(" \n what’s your father’s name\? ");
+return 0;
+}
