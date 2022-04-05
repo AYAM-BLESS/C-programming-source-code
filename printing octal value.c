323d2e08835f8d176f831975c9fbb356226e919a
AYAM-BLESS <84291403+AYAM-BLESS@users.noreply.github.com>
2022-04-05 13:45:30 +0100

    Add files via upload
    Printing octal values

---
+++ printing octal value.c
@@ -0,0 +1,11 @@
+// done by Ayamba Bless
+// printing octal value
+
+#include <stdio.h>
+int main ()
+{
+printf("\n below is the demo of  printing octal value");
+char* b = "B\124";
+printf(" \n%s", b);
+return 0;
+}
