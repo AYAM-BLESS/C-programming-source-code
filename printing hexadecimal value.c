6ee216e7480d6d6f07de6c3c8b36fed9faa919b5
AYAM-BLESS <84291403+AYAM-BLESS@users.noreply.github.com>
2022-04-05 13:44:50 +0100

    Add files via upload
    Printing hexadecimal value

---
+++ printing hexadecimal value.c
@@ -0,0 +1,11 @@
+// done by Ayamba Bless
+// printing hexa decimal value
+
+#include <stdio.h>
+int main ()
+{
+printf("\n Formatting output for hexadecimal value");
+char* s = "B\x5b";
+printf("\n %s", s);
+return 0;
+}
